4138dded8237d7eb4612816738d888957f07a426
unknown <niezwyklyable@gmail.com>
2025-03-15 17:11:16 +0100

    Developed modifying the task
     - developed two states of a task: to-do and completed
     - developed possibility to mark the task as completed
     - developed possibility to mark the task as to-do (e.g. in case incorrect marking)
     - developed possibility to change the target info of the task
     - developed possibility to delete the task from the list
     - added some colors in cmd output for readability using colorama module
    
    	modified:   README.md
    	modified:   main.py
    	modified:   task.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 from task import Task
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 tasks = [] # list of tasks, each task includes at least a target
 # and optionally a deadline or time/date info, a task is an instance of the class Task
 # so surely it could be more attributes and options in it
@@ -13,20 +16,85 @@ def main():
     while run:
         print()
         print('1. Show the list of all tasks.')
-        print('2. Add a task to the list.')
-        print('3. Exit.')
-        choosed_option = input('Please choose the number: ')
-        if choosed_option == '1':
+        print('2. Add a new task to the list.')
+        print('3. Mark the task as completed.')
+        print('4. Modify the task.')
+        print('5. Exit.')
+        chosen_option = input('Please choose the command: ').strip()
+
+        # Show the list of all tasks
+        if chosen_option == '1':
+            if len(tasks) == 0:
+                print('The list of tasks is empty.')
+                continue
             for t in tasks:
                 t.show_info()
-        elif choosed_option == '2':
-            target_info = input('Please enter the target info: ')
+
+        # Add a new task to the list
+        elif chosen_option == '2':
+            target_info = input('Please enter the target info: ').strip()
+            if len(target_info) == 0:
+                print(f'{Fore.RED}No info has detected. The task has not been added.')
+                continue
             task_counter += 1
             tasks.append(Task(task_counter, target_info))
-            print('The task has been added to the list properly.')
-        elif choosed_option == '3':
+            print(f'{Fore.GREEN}The task has been added to the list properly.')
+
+        # Mark the task as completed
+        elif chosen_option == '3':
+            try:
+                chosen_task_number = int(input('Please choose the task by its number: ').strip())
+            except ValueError:
+                print(f'{Fore.RED}Wrong input.')
+                continue
+            for t in tasks:
+                if t.task_number == chosen_task_number:
+                    t.mark_as_completed()
+                    print(f'{Fore.GREEN}Chosen task has been marked as completed.')
+                    break
+            else:
+                print(f'{Fore.RED}Wrong task number.')
+
+        # Modify the task
+        elif chosen_option == '4':
+            try:
+                chosen_task_number = int(input('Please choose the task by its number: ').strip())
+            except ValueError:
+                print(f'{Fore.RED}Wrong input.')
+                continue
+            for t in tasks:
+                if t.task_number == chosen_task_number:
+                    print('What do you want to do with the chosen task?')
+                    print('1. Change the target info.')
+                    print('2. Undo the command: mark the task as completed.')
+                    print('3. Delete the task from the list.')
+                    chosen_option = input('Please choose the command: ').strip()
+                    # Change the target info
+                    if chosen_option == '1':
+                        new_target_info = input(f'{t.target_info} --> ').strip()
+                        if len(new_target_info) == 0:
+                            print(f'{Fore.RED}No info has detected. Target info has not changed.')
+                            break
+                        t.change_target_info(new_target_info)
+                        print(f'{Fore.GREEN}Target info has changed properly.')
+                    # Undo the command: mark the task as completed
+                    elif chosen_option == '2':
+                        t.mark_as_to_do()
+                        print(f'{Fore.GREEN}Chosen task has been marked as to-do.')
+                    # Delete the task from the list
+                    elif chosen_option == '3':
+                        tasks.remove(t)
+                        print(f'{Fore.GREEN}Chosen task has been deleted from the list.')
+                    else:
+                        print(f'{Fore.RED}Not recognized command. Please try again.')
+                    break
+            else:
+                print(f'{Fore.RED}Wrong task number.')
+
+        # Exit the app
+        elif chosen_option == '5':
             run = False
         else:
-            print('Not recognized command. Please try again.')
+            print(f'{Fore.RED}Not recognized command. Please try again.')
 
 main()
--- task.py
+++ task.py
@@ -1,8 +1,26 @@
 
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
 class Task():
     def __init__(self, task_number, target_info):
-        self.task_number = task_number # needed to handle the specific task      
-        self.target_info = target_info
+        self.task_number = task_number # needed to handle the specific task, int type     
+        self.target_info = target_info # str type
+        self.state = 'to-do'
 
     def show_info(self):
-        print(f'Task no. {self.task_number}: {self.target_info}')
+        if self.state == 'to-do':
+            color = Fore.CYAN
+        elif self.state == 'completed':
+            color = Fore.BLUE
+        print(f'{color}Task no. {self.task_number}: {self.target_info} | [{self.state}]')
+
+    def mark_as_completed(self):
+        self.state = 'completed'
+
+    def mark_as_to_do(self):
+        self.state = 'to-do'
+
+    def change_target_info(self, new_target_info):
+        self.target_info = new_target_info
